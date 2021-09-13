c244be39c25ce81590fcb2018c92d8750e855eeb
kipariss1 <kpassudin@gmail.com>
2021-09-13 02:24:34 +0200

    feat (a2b_controller node/src/controller.py): Modifies class finction move2goal
    Modifies function move2goal from Turtlebot class, adds rotating to the goal to it

--- src/controller.py
+++ src/controller.py
@@ -3,6 +3,7 @@
 import rospy
 # importujeme metod "Twist" ktery bude posilat zpravy s tematem geometry_msgs
 from geometry_msgs.msg import Twist
+from math import atan2
 
 
 
@@ -12,6 +13,10 @@ class TurtleBot:
         rospy.init_node("speed_publisher")     # inicializace nodu s nazvem nodu
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1) # definice publisheru, ktery bude posilat zpravy a turtlebot bude poslouchat tyto zpravy
         self.rate = rospy.Rate(2) # posilat zpravy s frekvenci 2 ms 
+
+
+    def goal_angle(self, goal_coord_arr, orig_coord_arr):
+        return atan2(goal_coord_arr["x"] - orig_coord_arr["x"], goal_coord_arr["y"] - orig_coord_arr["y"])
 
 
     def move2goal(self):
@@ -24,6 +29,26 @@ class TurtleBot:
 
         vel_msg = Twist()   # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni rychlosti turtlebotu
         
+
+        ####OTACENI####
+
+        angle = self.goal_angle(goal_coord_arr, orig_coord_arr)
+        t0 = rospy.Time.now().to_sec()   # pro zaznamenani casu
+        t1 = t0
+        ang_vel = 0.1                    # uhlova rychlos
+
+        while (ang_vel*(t1-t0)<angle):
+            vel_msg.angular.z = ang_vel
+            self.pub.publish(vel_msg)
+            t1 =  rospy.Time.now().to_sec()
+
+        vel_msg.angular.z = 0.0
+        self.pub.publish(vel_msg)
+
+        ###############
+
+        #####JIZDA####
+
         i = 0
 
         while i<20:
@@ -35,6 +60,8 @@ class TurtleBot:
         # podminka dosazeni cile
         vel_msg.linear.x = 0.0 
         self.pub.publish(vel_msg)
+
+        ##############
 
         rospy.spin()    # ctrl+c zastavi node
 
