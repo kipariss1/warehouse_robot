43a376ea7ca84184c857048e9ba5b305d6c5d729
kipariss1 <kpassudin@gmail.com>
2021-09-13 03:15:37 +0200

    feat (a2b_controller/src/controller.py): Adds moving from a2b
    Modifies function move2goal, so the robot correctly goes to the goal based
    on input coordinates

--- src/controller.py
+++ src/controller.py
@@ -3,7 +3,7 @@
 import rospy
 # importujeme metod "Twist" ktery bude posilat zpravy s tematem geometry_msgs
 from geometry_msgs.msg import Twist
-from math import atan2
+from math import atan2, pow, sqrt
 
 
 
@@ -17,6 +17,10 @@ class TurtleBot:
 
     def goal_angle(self, goal_coord_arr, orig_coord_arr):
         return atan2(goal_coord_arr["x"] - orig_coord_arr["x"], goal_coord_arr["y"] - orig_coord_arr["y"])
+
+
+    def goal_distance(self, goal_coord_arr, orig_coord_arr):
+        return sqrt(pow((goal_coord_arr["x"] - orig_coord_arr["x"]), 2) + pow((goal_coord_arr["y"] - orig_coord_arr["y"]), 2))
 
 
     def move2goal(self):
@@ -37,8 +41,11 @@ class TurtleBot:
         t1 = t0
         ang_vel = 0.1                    # uhlova rychlos
 
-        while (ang_vel*(t1-t0)<angle):
-            vel_msg.angular.z = ang_vel
+        while (ang_vel*(t1-t0)<abs(angle)):
+            if (angle<=0):
+                vel_msg.angular.z = -ang_vel
+            else:
+                vel_msg.angular.z = ang_vel
             self.pub.publish(vel_msg)
             t1 =  rospy.Time.now().to_sec()
 
@@ -49,13 +56,16 @@ class TurtleBot:
 
         #####JIZDA####
 
-        i = 0
+        distance = self.goal_distance(goal_coord_arr, orig_coord_arr)
+        t0 = rospy.Time.now().to_sec()   # pro zaznamenani casu
+        t1 = t0
+        lin_vel = 0.1                    # linearni rychlos
 
-        while i<20:
-            vel_msg.linear.x = 0.5 # pohyb podel osy x s rychlosti 0.5
+        while (lin_vel*(t1-t0)<distance):
+            vel_msg.linear.x = lin_vel # pohyb podel osy x s rychlosti 0.5
             self.pub.publish(vel_msg)
             self.rate.sleep()
-            i += 1
+            t1 = rospy.Time.now().to_sec()
 
         # podminka dosazeni cile
         vel_msg.linear.x = 0.0 
