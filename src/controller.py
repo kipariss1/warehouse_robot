e5c08b50b92bc73b832e1bfb911a9944916f4520
kipariss1 <kpassudin@gmail.com>
2021-10-11 00:56:19 +0200

    feat (src/controller.py): Adds usage of sensors
    Adds usage of laser scanner of turtlebot for obstacle avoidance.
    naive_obstacle_avoidance is not implemented yet

--- src/controller.py
+++ src/controller.py
@@ -4,23 +4,25 @@ import rospy
 # importujeme metod "Twist" ktery bude posilat zpravy s tematem geometry_msgs
 from geometry_msgs.msg import Twist
 from math import atan2, pow, sqrt
+# importujeme metod "Laser scan" ktery bude koukat na hodnoty ze senzoru
 from sensor_msgs.msg import LaserScan
 
 
 
 
 def callback(msg):
-    	
-    print(msg)
-
+	global distance_obst_arr
+	distance_obst_arr = {"0 deg":msg.ranges[0]}		# globalni dictionary do ktereho se zapisuji hodnoty ze sensoru, kolik zbylo do prekazky pos uhlem 0
 
 
 class TurtleBot:
 
     def __init__(self):
         rospy.init_node("speed_publisher")     # inicializace nodu s nazvem nodu
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1) 	# definice publisheru, ktery bude posilat zpravy a turtlebot bude poslouchat tyto zpravy
-        self.sub = rospy.Subscriber("/scan", LaserScan, callback)		# definice subscriberu, ktery bude sledovat tema /scan
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1) 	# definice publisheru, ktery bude posilat zpravy a turtlebot bude poslouchat tyto 											# zpravy
+        
+        self.sub = rospy.Subscriber("/scan", LaserScan, callback)		# definice subscriberu, ktery bude sledovat tema /scan, tema s kterou publikuji
+        									# senzory turtlebotu
         self.rate = rospy.Rate(2) # posilat zpravy s frekvenci 2 ms 
 
 
@@ -32,14 +34,11 @@ class TurtleBot:
         return sqrt(pow((goal_coord_arr["x"] - orig_coord_arr["x"]), 2) + pow((goal_coord_arr["y"] - orig_coord_arr["y"]), 2))
         
         
-    def obstacle_avoidance(self, dist_from_orig):
+    def naive_obstacle_avoidance(self, dist_from_orig):
     
-    	# TODO vypocet novych sour + callback 
+    	# TODO vypocet novych sour 
     	
     	pass    	
-    
-    
-    # TODO definovat callback 
 
 
     def move2goal(self):
@@ -81,10 +80,12 @@ class TurtleBot:
         lin_vel = 0.1                    # linearni rychlos
 
         while (lin_vel*(t1-t0)<distance):
-            vel_msg.linear.x = lin_vel # pohyb podel osy x s rychlosti 0.5
+            vel_msg.linear.x = lin_vel # pohyb podel osy x robota s rychlosti 0.5
             self.pub.publish(vel_msg)
             self.rate.sleep()
-            t1 = rospy.Time.now().to_sec()
+            t1 = rospy.Time.now().to_sec()    
+            if (distance_obst_arr["0 deg"]<0.8):
+            	print("I've noticed ya!")		# TODO: implement here function naive_obstacle_avoidance            	
 
         # podminka dosazeni cile
         vel_msg.linear.x = 0.0 
