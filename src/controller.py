c071945766e3f75ad1191f4a9dcc7f3764243e6f
kipariss1 <kpassudin@gmail.com>
2021-12-20 15:59:52 +0100

    add (src/controller.py): Not successful try of proper exit
    Not yet successful try of implementing of proper exit in terminal
    using "ctrl+c"

--- src/controller.py
+++ src/controller.py
@@ -7,6 +7,7 @@ from math import atan2, pow, sqrt, pi
 # importujeme metod "Laser scan" ktery bude koukat na hodnoty ze senzoru
 from sensor_msgs.msg import LaserScan
 from typing import Any, Dict
+import sys
 
 
 def callback(msg):
@@ -47,128 +48,162 @@ class TurtleBot:
 
     def rotation_move(self, angle, ang_vel):
 
-        vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values -
-        # uhlove a linearni rychlosti turtlebotu
+        try:
 
-        t0 = rospy.Time.now().to_sec()  # pro zaznamenani casu
-        t1 = t0
+            vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values -
+            # uhlove a linearni rychlosti turtlebotu
 
-        while ang_vel * (t1 - t0) < abs(angle):
-            if angle <= 0:
-                vel_msg.angular.z = -ang_vel
-            else:
-                vel_msg.angular.z = ang_vel
+            t0 = rospy.Time.now().to_sec()  # pro zaznamenani casu
+            t1 = t0
+
+            while ang_vel * (t1 - t0) < abs(angle):
+                if angle <= 0:
+                    vel_msg.angular.z = -ang_vel
+                else:
+                    vel_msg.angular.z = ang_vel
+                self.pub.publish(vel_msg)
+                t1 = rospy.Time.now().to_sec()
+
+            vel_msg.angular.z = 0.0
             self.pub.publish(vel_msg)
-            t1 = rospy.Time.now().to_sec()
 
-        vel_msg.angular.z = 0.0
-        self.pub.publish(vel_msg)
+        except rospy.ROSInterruptException:
+            return None
 
-        return
+        return None
 
     def linear_move(self, distance, lin_vel):
 
-        obstacle_avoided: bool = False
-        curr_length_of_path: float = 0
-        length_of_obstacle: float = 0
+        try:
 
-        vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
-        # rychlosti turtlebotu
+            obstacle_avoided: bool = False
+            curr_length_of_path: float = 0
+            length_of_obstacle: float = 0
 
-        t0 = rospy.Time.now().to_sec()  # pro zaznamenani casu
-        t1 = t0
+            vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
+            # rychlosti turtlebotu
 
-        while (lin_vel * (t1 - t0) < distance):
-            vel_msg.linear.x = lin_vel  # pohyb podel osy x robota s rychlosti 0.5
+            t0 = rospy.Time.now().to_sec()  # pro zaznamenani casu
+            t1 = t0
+
+            while (lin_vel * (t1 - t0) < distance):
+                vel_msg.linear.x = lin_vel  # pohyb podel osy x robota s rychlosti 0.5
+                self.pub.publish(vel_msg)
+                self.rate.sleep()
+                t1 = rospy.Time.now().to_sec()
+                # podminka prekazky
+                if distance_obst_arr["0 deg"] < self.safety_boundary:
+                    curr_length_of_path = lin_vel*(t1-t0)
+                    obstacle_avoided, _ = self.naive_obstacle_avoidance()
+                    while not obstacle_avoided:
+                        obstacle_avoided, length_of_obstacle = self.naive_obstacle_avoidance()
+                    break
+
+            # podminka dosazeni cile
+            vel_msg.linear.x = 0.0
             self.pub.publish(vel_msg)
-            self.rate.sleep()
-            t1 = rospy.Time.now().to_sec()
-            # podminka prekazky
-            if distance_obst_arr["0 deg"] < self.safety_boundary:
-                curr_length_of_path = lin_vel*(t1-t0)
-                obstacle_avoided, _ = self.naive_obstacle_avoidance()
-                while not obstacle_avoided:
-                    obstacle_avoided, length_of_obstacle = self.naive_obstacle_avoidance()
-                break
 
-        # podminka dosazeni cile
-        vel_msg.linear.x = 0.0
-        self.pub.publish(vel_msg)
+            # dojet cil
+            if obstacle_avoided:
+                self.linear_move(distance - curr_length_of_path - length_of_obstacle, self.lin_vel)
 
-        # dojet cil
-        if obstacle_avoided:
-            self.linear_move(distance - curr_length_of_path - length_of_obstacle, self.lin_vel)
-
+        except rospy.ROSInterruptException:
+            return None
 
     def naive_obstacle_avoidance(self):
 
-        obstacle_avoided: bool = False  # Flag, that obstacle is avoided
-        deviation_from_x: float = 0
-        deviation_from_y: float = 0
+        try:
+            obstacle_avoided: bool = False  # Flag, that obstacle is avoided
+            deviation_from_x: float = 0
+            deviation_from_y: float = 0
 
-        vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
-        # rychlosti turtlebotu
+            vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
+            # rychlosti turtlebotu
 
-        vel_msg.linear.x = 0.0
-        vel_msg.angular.z = 0.0
-        self.pub.publish(vel_msg)
+            vel_msg.linear.x = 0.0
+            vel_msg.angular.z = 0.0
+            self.pub.publish(vel_msg)
 
-        # otoceni vlevo
-        self.rotation_move(pi/2, self.ang_vel)
+            # otoceni vlevo
+            self.rotation_move(pi/2, self.ang_vel)
 
-        # jizda pokud neprejede prekazku
-        while distance_obst_arr["270 deg"] <= self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
-            deviation_from_y += 0.7
+            # jizda pokud neprejede prekazku
+            while distance_obst_arr["270 deg"] <= self.safety_boundary:
+                self.linear_move(0.7, self.lin_vel)
+                deviation_from_y += 0.7
 
-        # otoceni vlevo
-        self.rotation_move(-pi / 2, self.ang_vel)
+            # otoceni vlevo
+            self.rotation_move(-pi / 2, self.ang_vel)
 
-        # jizda pokud zase nedojede do prekazky
-        while distance_obst_arr["270 deg"] > self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
+            # jizda pokud zase nedojede do prekazky
+            while distance_obst_arr["270 deg"] > self.safety_boundary:
+                self.linear_move(0.7, self.lin_vel)
 
-        # jizda pokud zase neprejede prekazku
-        while distance_obst_arr["270 deg"] <= self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
-            deviation_from_x += 0.7
+            # jizda pokud zase neprejede prekazku
+            while distance_obst_arr["270 deg"] <= self.safety_boundary:
+                self.linear_move(0.7, self.lin_vel)
+                deviation_from_x += 0.7
 
-        # otoceni vlevo
-        self.rotation_move(-pi / 2, self.ang_vel)
+            # otoceni vlevo
+            self.rotation_move(-pi / 2, self.ang_vel)
 
-        # jizda zpadky na cestu
-        self.linear_move(deviation_from_y, self.lin_vel)
+            # jizda zpadky na cestu
+            self.linear_move(deviation_from_y, self.lin_vel)
 
-        # otoceni na spravny smer
-        self.rotation_move(pi / 2, self.ang_vel)
+            # otoceni na spravny smer
+            self.rotation_move(pi / 2, self.ang_vel)
 
-        obstacle_avoided = True
+            obstacle_avoided = True
+
+        except rospy.ROSInterruptException:
+            return None
 
         return obstacle_avoided, deviation_from_x
 
     def move2goal(self):
-        goal_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole souradnic cile
-        orig_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole pocatecnich souradnic
 
-        # zadani souradnic cile
-        goal_coord_arr["x"] = float(input("Souradnice cile x: "))
-        goal_coord_arr["y"] = float(input("Souradnice cile y: "))
+        try:
+            goal_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole souradnic cile
+            orig_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole pocatecnich souradnic
 
-        ####OTACENI####
-        angle = self.goal_angle(goal_coord_arr, orig_coord_arr)
+            # zadani souradnic cile
+            goal_coord_arr["x"] = float(input("Souradnice cile x: "))
+            goal_coord_arr["y"] = float(input("Souradnice cile y: "))
 
-        self.rotation_move(angle, self.ang_vel)
-        ###############
+            ####OTACENI####
+            angle = self.goal_angle(goal_coord_arr, orig_coord_arr)
 
-        #####JIZDA####
-        distance = self.goal_distance(goal_coord_arr, orig_coord_arr)
+            self.rotation_move(angle, self.ang_vel)
+            ###############
 
-        self.linear_move(distance, self.lin_vel)
-        ##############
+            #####JIZDA####
+            distance = self.goal_distance(goal_coord_arr, orig_coord_arr)
 
-        rospy.spin()  # ctrl+c zastavi node
+            self.linear_move(distance, self.lin_vel)
+            ##############
+
+            # rospy.spin()  # ctrl+c zastavi node
+
+        except rospy.ROSInterruptException:
+            return None
 
 
-while not rospy.is_shutdown():
+def main():
+
     x = TurtleBot()
-    x.move2goal()
+
+    try:
+        x.move2goal()
+        rate.sleep()
+    except rospy.ROSInterruptException:
+        rospy.logerr("Shutting down a2b_controller")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
+
+
+# while not rospy.is_shutdown():
+#     x = TurtleBot()
+#     x.move2goal()
