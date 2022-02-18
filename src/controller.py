599ead7adc40efd3d5f0b1d9cd0cd68c7a7fba4a
kipariss1 <kpassudin@gmail.com>
2022-02-18 15:38:18 +0100

    fix (src/controller): add intervals
    Adds standard deviation to the measuring of distance on the right side of
    the robot during naive obstacle avoidance algorithm

--- src/controller.py
+++ src/controller.py
@@ -20,7 +20,6 @@ def callback(msg):
 class TurtleBot:
 
     def __init__(self):
-        rospy.init_node("speed_publisher")  # inicializace nodu s nazvem nodu
 
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)  # definice publisheru,
         # ktery bude posilat zpravy a turtlebot
@@ -32,9 +31,12 @@ class TurtleBot:
 
         self.rate = rospy.Rate(2)  # posilat zpravy s frekvenci 2 ms
 
-        self.safety_boundary = 0.5
+        self.safety_boundary = 0.4
         self.lin_vel = 0.1
-        self.ang_vel = 0.1
+        self.ang_vel = 0.2
+
+        self.safe_dx = 0.2
+        self.variance_of_distance = 0.2
 
     @staticmethod  # does not use self in it
     def goal_angle(goal_coord_arr, orig_coord_arr):
@@ -47,6 +49,8 @@ class TurtleBot:
 
     def rotation_move(self, angle, ang_vel):
 
+        print("Moving angular with", ang_vel, "speed")  # debug
+
         vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values -
         # uhlove a linearni rychlosti turtlebotu
 
@@ -54,19 +58,35 @@ class TurtleBot:
         t1 = t0
 
         while ang_vel * (t1 - t0) < abs(angle):
+
+            #################
+            ## NAIVE STOP! ##
+            #################
+            if rospy.is_shutdown():
+                self.stop_tbot()
+                self.rate.sleep()
+                return None
+            #################
+            ## NAIVE STOP! ##
+            #################
+
             if angle <= 0:
                 vel_msg.angular.z = -ang_vel
             else:
                 vel_msg.angular.z = ang_vel
             self.pub.publish(vel_msg)
             t1 = rospy.Time.now().to_sec()
+            self.rate.sleep()
 
         vel_msg.angular.z = 0.0
         self.pub.publish(vel_msg)
+        self.rate.sleep()
 
-        return
+        return None
 
     def linear_move(self, distance, lin_vel):
+
+        print("Moving linear with", lin_vel, "speed") # debug
 
         obstacle_avoided: bool = False
         curr_length_of_path: float = 0
@@ -79,12 +99,25 @@ class TurtleBot:
         t1 = t0
 
         while (lin_vel * (t1 - t0) < distance):
+
+            #################
+            ## NAIVE STOP! ##
+            #################
+            if rospy.is_shutdown():
+                self.stop_tbot()
+                self.rate.sleep()
+                return None
+            #################
+            ## NAIVE STOP! ##
+            #################
+
             vel_msg.linear.x = lin_vel  # pohyb podel osy x robota s rychlosti 0.5
             self.pub.publish(vel_msg)
             self.rate.sleep()
             t1 = rospy.Time.now().to_sec()
             # podminka prekazky
             if distance_obst_arr["0 deg"] < self.safety_boundary:
+                print("obstacle detected in ", distance_obst_arr["0 deg"])
                 curr_length_of_path = lin_vel*(t1-t0)
                 obstacle_avoided, _ = self.naive_obstacle_avoidance()
                 while not obstacle_avoided:
@@ -99,12 +132,43 @@ class TurtleBot:
         if obstacle_avoided:
             self.linear_move(distance - curr_length_of_path - length_of_obstacle, self.lin_vel)
 
+        return None
+
+    def stop_tbot(self):
+
+        print("stop tbot")
+
+        vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
+        # rychlosti turtlebotu
+
+        vel_msg.linear.x = 0
+
+        vel_msg.angular.z = 0
+
+        self.pub.publish(vel_msg)
+        self.rate.sleep()
+
+        return None
 
     def naive_obstacle_avoidance(self):
+
+        print("naive obstacle avoidance")
+
+        #################
+        ## NAIVE STOP! ##
+        #################
+        if rospy.is_shutdown():
+            self.stop_tbot()
+            self.rate.sleep()
+            return None
+        #################
+        ## NAIVE STOP! ##
+        #################
 
         obstacle_avoided: bool = False  # Flag, that obstacle is avoided
         deviation_from_x: float = 0
         deviation_from_y: float = 0
+        residue_of_avoidance = 0.9     # error of avoidance due to odometry (very big error!!)
 
         vel_msg = Twist()  # definice objektu zpravy uvnitr ktereho budou linear a angular values - uhlove a linearni
         # rychlosti turtlebotu
@@ -116,27 +180,33 @@ class TurtleBot:
         # otoceni vlevo
         self.rotation_move(pi/2, self.ang_vel)
 
-        # jizda pokud neprejede prekazku
-        while distance_obst_arr["270 deg"] <= self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
-            deviation_from_y += 0.7
+        # jizda pokud neprejede prekazku (pokud je v intervalu bezpecne distance)
+        while (distance_obst_arr["270 deg"] <= (self.safety_boundary + self.variance_of_distance)) \
+                and (distance_obst_arr["270 deg"] >= (self.safety_boundary - self.variance_of_distance)):
+            print("first distance with safe margin, ", distance_obst_arr["270 deg"])
+            self.linear_move(self.safe_dx, self.lin_vel)
+            deviation_from_y += self.safe_dx
 
         # otoceni vlevo
         self.rotation_move(-pi / 2, self.ang_vel)
 
-        # jizda pokud zase nedojede do prekazky
-        while distance_obst_arr["270 deg"] > self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
+        # jizda pokud zase nedojede do prekazky (mimo interval bezpeci)
+        while distance_obst_arr["270 deg"] > (self.safety_boundary + self.variance_of_distance):
+            print("second distance with safe margin, ", distance_obst_arr["270 deg"])
+            self.linear_move(self.safe_dx, self.lin_vel)
 
-        # jizda pokud zase neprejede prekazku
-        while distance_obst_arr["270 deg"] <= self.safety_boundary:
-            self.linear_move(0.7, self.lin_vel)
-            deviation_from_x += 0.7
+        # jizda pokud zase neprejede prekazku (pokud je v intervalu bezpecne distance)
+        while (distance_obst_arr["270 deg"] <= (self.safety_boundary + self.variance_of_distance)) \
+                and (distance_obst_arr["270 deg"] >= (self.safety_boundary - self.variance_of_distance)):
+            print("third distance with safe margin, ", distance_obst_arr["270 deg"])
+            self.linear_move(self.safe_dx, self.lin_vel)
+            deviation_from_x += self.safe_dx
 
-        # otoceni vlevo
+        # otoceni vpravo
         self.rotation_move(-pi / 2, self.ang_vel)
 
         # jizda zpadky na cestu
+        print("forth distance with safe margin, ", distance_obst_arr["270 deg"])
         self.linear_move(deviation_from_y, self.lin_vel)
 
         # otoceni na spravny smer
@@ -147,6 +217,20 @@ class TurtleBot:
         return obstacle_avoided, deviation_from_x
 
     def move2goal(self):
+
+        print("move2goal")
+
+        #################
+        ## NAIVE STOP! ##
+        #################
+        if rospy.is_shutdown():
+            self.stop_tbot()
+            self.rate.sleep()
+            return None
+        #################
+        ## NAIVE STOP! ##
+        #################
+
         goal_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole souradnic cile
         orig_coord_arr: Dict[str, Any] = {"x": 0.0, "y": 0.0}  # definice pole pocatecnich souradnic
 
@@ -166,9 +250,17 @@ class TurtleBot:
         self.linear_move(distance, self.lin_vel)
         ##############
 
-        rospy.spin()  # ctrl+c zastavi node
+
+def main():
+
+    rospy.init_node("controller_py_n")  # inicializace nodu s nazvem nodu
+
+    tb = TurtleBot()
+    tb.move2goal()
+    if rospy.is_shutdown():
+        tb.stop_tbot()
+    #rospy.spin()
 
 
-while not rospy.is_shutdown():
-    x = TurtleBot()
-    x.move2goal()
+if __name__ == '__main__':
+    main()
